3fadc42e87e25174e23e907e582fabed933c5655
yegoncollins <87011989+yegoncollins@users.noreply.github.com>
2022-10-08 15:30:06 +0300

    init

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,21 +1,48 @@
-import { useState, useEffect} from 'react';
-import { Box, Stack, Topography } from '@mui/material';
-import 
+import React, { useEffect, useState } from "react";
+import { Box, Stack, Typography } from "@mui/material";
 
-const Feed = () => {
-  return (
-    <Stack sx={{ flexDirection: { sx: "column", md: "row"}}}>
-      <Box sx={{ height: {sx: 'auto', md: '92vh'}, borderRight: '1px solid #3d3d3d', px: {sx: 0, md: 2}}}>
-        SideBar
-    <Topography className="copyright" variant="body2" sx={{ mt: 1.5, color: '#fff' }}>
-      Copyright 2022 yegoncollins
 
-    </Topography>
+import { fetchFromAPI } from "../utils/fetchFromAPI";
+import { SideBar, Videos} from './';
+
+
+  const Feed = () => {
+
+    const[selectedCategory,
+    setSelectedCategory] = useState('New');
+
+    useEffect(() => {
+      fetchFromAPI(`search?part=snippet&q=$
+      {selectedCategory}`);
+     }, [selectedCategory]);
+
+    return (
+      <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
+      <Box sx={{ height: { sx: "auto", md: "92vh" }, borderRight: "1px solid #3d3d3d", px: { sx: 0, md: 2 } }}>
+
+        <SideBar
+        selectedCategory=
+        {selectedCategory}
+        setSelectedCategory=
+        {setSelectedCategory}
+        />
+
+        <Typography className="copyright" variant="body2" sx={{ mt: 1.5, color: "#fff", }}>
+          Copyright © 2022 yegoncollins
+        </Typography>
+      </Box>
+
+      <Box p= {2} sx= {{ overflowY:'auto', height: '90vh', flex: 2}}>
+        <Typography variant= "h4" fontWeight= "bold" mb={2} sx= {{color: 'white'}}>
+          {selectedCategory} <span style={{color: '#F31503'}}>videos</span>
+        </Typography>
+
+        <Videos videos={[]}/>
 
       </Box>
 
-    </Stack>
-  )
-}
+      </Stack>
+    );
+  }
 
 export default Feed
